0a8f5c7a85d5c0ef2f2009d8b1ac97b761dfbff4
Roma <r.dyug.perfectorium@gmail.com>
2018-08-11 17:52:29 +0300

    start project

--- server/index.js
+++ server/index.js
@@ -1,1 +1,12 @@
-console.log('welcome');
+const fs = require('fs');
+const http = require('http');
+
+
+const port = 3000;
+const server = http.createServer(function (req,res) {
+const pathBaseTemp = './template/index.html';
+const streamBaseTemp = fs.ReadStream(pathBaseTemp);
+streamBaseTemp.pipe(res);
+});
+
+server.listen(port,()=>console.log(`I work in  ${port} port`));
