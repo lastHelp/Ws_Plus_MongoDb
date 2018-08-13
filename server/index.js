69121316d6330b23bd89612f88745943d3379f85
Roma <r.dyug.perfectorium@gmail.com>
2018-08-13 05:05:37 +0300

    final version

--- server/index.js
+++ server/index.js
@@ -1,6 +1,34 @@
 
 const http = require('http');
-const controller = require('../controller')
-
+const controller = require('../controller');
+const socket =  require("socket.io");
+const app = http .createServer(controller);
+const io = socket.listen(app);
+const DbStorage = require('../lib/class/bgStorageS');
 const port = 3000;
-http.createServer(controller).listen(port,()=>console.log(`I work in  ${port} port`));
+
+app.listen(port,()=>console.log(`I work in  ${port} port`));
+
+
+io.sockets.on('connection',async  function (socket) {
+    console.log('new user conect');
+    const dbName = "random_text_storage";
+    const collectName = ["collect1","collect2"];
+
+    const randomTextStorage = new DbStorage(dbName,...collectName);
+    await randomTextStorage.conect();
+    await (async function () {
+    while(true) {
+      const response = await randomTextStorage.data();
+      const sendData = JSON.stringify(response);
+        socket.emit('getRow', sendData );
+     if(!response) {
+        socket.emit('finally', {});
+         break
+     }
+  }
+  })();
+	socket.on('disconnect', function () {
+		console.log('user disconnected');
+	});
+});
